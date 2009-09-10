8ac67a84b28a46867bce1d66739fd99d13a52fc1
David Waite <david@alkaline-solutions.com>
2009-09-10 00:04:58 -0600

    Added ability for validate_cookie (and the InvalidCookieError) to return multiple failure messages on a given cookie.

--- lib/cookiejar/cookie.rb
+++ lib/cookiejar/cookie.rb
@@ -112,6 +112,8 @@ module CookieJar
       request_secure = (uri.scheme == 'https')
       cookie_host = cookie.domain
       cookie_path = cookie.path
+      
+      errors = []
     
       # From RFC 2965, Section 3.3.2 Rejecting Cookies
     
@@ -119,18 +121,18 @@ module CookieJar
       # Version attribute is missing. Note that the legacy Set-Cookie
       # directive will result in an implicit version 0.
       unless cookie.version
-        raise InvalidCookieError, "Cookie version not supplied (or implicit with Set-Cookie)"
+        errors << "Version missing"
       end
 
       # The value for the Path attribute is not a prefix of the request-URI
       unless request_path.start_with? cookie_path 
-        raise InvalidCookieError, "Cookie path should match or be a subset of the request path"
+        errors << "Path is not a prefix of the request uri path"
       end
 
-      # The value for the Domain attribute contains no embedded dots, and the value is not .local
-      # Note: we also allow IPv4 and IPv6 addresses
-      unless cookie_host =~ IPADDR || cookie_host =~ /.\../ || cookie_host == '.local'
-        raise InvalidCookieError, "Cookie domain format is not legal"
+      unless cookie_host =~ IPADDR || #is an IPv4 or IPv6 address
+        cookie_host =~ /.\../ || #contains an embedded dot
+        cookie_host == '.local' #is the domain cookie for local addresses
+        errors << "Domain format is illegal"
       end
     
       # The effective host name that derives from the request-host does
@@ -141,17 +143,19 @@ module CookieJar
       # that contains one or more dots.
       effective_host = effective_host uri
       unless domains_match effective_host, cookie_host
-        raise InvalidCookieError, "Cookie domain is inappropriate based on request hostname"
+        errors << "Domain is inappropriate based on request URI hostname"
       end
     
       # The Port attribute has a "port-list", and the request-port was
       # not in the list.
       unless cookie.ports.nil? || cookie.ports.length != 0
         unless cookie.ports.find_index uri.port
-          raise InvalidCookieError, "incoming request port does not match cookie port(s)"
+          errors << "Ports list does not contain request URI port"
         end
       end
-    
+
+      raise InvalidCookieError.new (errors) unless errors.empty?
+
       # Note: 'secure' is not explicitly defined as an SSL channel, and no
       # test is defined around validity and the 'secure' attribute
       true
--- lib/cookiejar/cookie_common.rb
+++ lib/cookiejar/cookie_common.rb
@@ -1,4 +1,13 @@
 module CookieJar
   class CookieError < StandardError; end
-  class InvalidCookieError < CookieError; end
+  class InvalidCookieError < CookieError; 
+    attr_reader :messages
+    def initialize message
+     if message.is_a? Array
+       @messages = message
+       message = message.join ', '
+     end
+     super(message)
+    end
+  end
 end
